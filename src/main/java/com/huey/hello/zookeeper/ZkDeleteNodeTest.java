e9bfff1591fa331446efb269dff68164b7edd0f0
huey <huey2672@163.com>
2018-05-24 23:36:33 +0800

    ZkCreateNodeTest

---
+++ src/main/java/com/huey/hello/zookeeper/ZkDeleteNodeTest.java
@@ -0,0 +1,32 @@
+package com.huey.hello.zookeeper;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.Watcher.Event.KeeperState;
+
+/**
+ * 以同步的方式删除节点的示例
+ * @author huey
+ */
+public class ZkDeleteNodeTest {
+	public static void main(String[] args) throws Exception {
+		final CountDownLatch connectedSignal = new CountDownLatch(1);
+		ZooKeeper zk = new ZooKeeper("127.0.0.1:2181", 5000, new Watcher() {
+			@Override
+			public void process(WatchedEvent event) {
+				if (event.getState() == KeeperState.SyncConnected) {
+					connectedSignal.countDown();
+				}
+			}
+		});
+		connectedSignal.await();
+		
+		// 版本指定 -1，表示客户端基于数据的最新版本操作
+		zk.delete("/zk-huey", -1);
+		
+		zk.close();
+	}
+}
