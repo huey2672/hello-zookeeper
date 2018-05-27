60aa6926632fdae2a33f3628678bd505af84e51b
huey <huey2672@163.com>
2018-05-27 16:08:41 +0800

    ZkClientReadDataTest

---
+++ src/main/java/com/huey/hello/zookeeper/zkclient/ZkClientReadDataTest.java
@@ -0,0 +1,25 @@
+package com.huey.hello.zookeeper.zkclient;
+
+import org.I0Itec.zkclient.ZkClient;
+
+/**
+ * 获取节点的数据内容
+ * @author huey
+ */
+public class ZkClientReadDataTest {
+
+	public static void main(String[] args) {
+		ZkClient zkClient = new ZkClient("127.0.0.1:2181", 5000);
+
+		// 获取节点的数据内容
+		String data = zkClient.readData("/zkclient-nonexist", true);
+		if (data != null) {
+			System.out.println("Node Data: " + data);
+		} else {
+			System.out.println("The node is nonexistent.");
+		}
+		
+		zkClient.close();
+	}
+
+}
--- src/main/java/com/huey/hello/zookeeper/zkclient/ZkClientWriteDataTest.java
+++ src/main/java/com/huey/hello/zookeeper/zkclient/ZkClientWriteDataTest.java
@@ -3,7 +3,7 @@ package com.huey.hello.zookeeper.zkclient;
 import org.I0Itec.zkclient.ZkClient;
 
 /**
- * 更新数据
+ * 更新节点的数据内容
  * 
  * @author huey
  */
@@ -12,6 +12,7 @@ public class ZkClientWriteDataTest {
 	public static void main(String[] args) {
 		ZkClient zkClient = new ZkClient("127.0.0.1:2181", 5000);
 		
+		// 更新节点的数据内容
 		zkClient.writeData("/zkclient", "hello");
 		
 		zkClient.close();
