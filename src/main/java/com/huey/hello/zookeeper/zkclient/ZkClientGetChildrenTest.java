62b1b437bcee699120c019f231a9cd3d30078c3c
huey <huey2672@163.com>
2018-05-27 16:25:36 +0800

    add comment

--- src/main/java/com/huey/hello/zookeeper/zkclient/ZkClientGetChildrenTest.java
+++ src/main/java/com/huey/hello/zookeeper/zkclient/ZkClientGetChildrenTest.java
@@ -14,6 +14,7 @@ public class ZkClientGetChildrenTest {
 	public static void main(String[] args) {
 		ZkClient zkClient = new ZkClient("127.0.0.1:2181", 5000);
 
+		// 获取子节点列表
 		List<String> children = zkClient.getChildren("/");
 		System.out.println(children);
 
